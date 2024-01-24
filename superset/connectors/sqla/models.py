8237798a30468fb109038e9ab447e10f004625fd
Beto Dealmeida <roberto@dealmeida.net>
2024-01-24 09:02:24 -0500

    Update tests

--- superset/connectors/sqla/models.py
+++ superset/connectors/sqla/models.py
@@ -104,7 +104,6 @@ from superset.models.helpers import (
     ExploreMixin,
     ImportExportMixin,
     QueryResult,
-    QueryStringExtended,
     validate_adhoc_subquery,
 )
 from superset.models.slice import Slice
