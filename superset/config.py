b7af24dc4e903baf7ec8f713e21f498d0d1ed9a8
SengWei <csengwei96@gmail.com>
2024-03-03 20:20:16 +0800

    fast commit

--- superset/config.py
+++ superset/config.py
@@ -88,7 +88,6 @@ else:
 VERSION_INFO_FILE = str(files("superset") / "static/version_info.json")
 PACKAGE_JSON_FILE = str(files("superset") / "static/assets/package.json")
 
-
 # Multiple favicons can be specified here. The "href" property
 # is mandatory, but "sizes," "type," and "rel" are optional.
 # For example:
@@ -531,11 +530,10 @@ SSH_TUNNEL_TIMEOUT_SEC = 10.0
 #: Timeout (seconds) for transport socket (``socket.settimeout``)
 SSH_TUNNEL_PACKET_TIMEOUT_SEC = 1.0
 
-
 # Feature flags may also be set via 'SUPERSET_FEATURE_' prefixed environment vars.
 DEFAULT_FEATURE_FLAGS.update(
     {
-        k[len("SUPERSET_FEATURE_") :]: parse_boolean_string(v)
+        k[len("SUPERSET_FEATURE_"):]: parse_boolean_string(v)
         for k, v in os.environ.items()
         if re.search(r"^SUPERSET_FEATURE_\w+", k)
     }
@@ -933,6 +931,7 @@ DASHBOARD_AUTO_REFRESH_INTERVALS = [
 # celery beat triggered it, see https://github.com/celery/celery/issues/6974 for details
 CELERY_BEAT_SCHEDULER_EXPIRES = timedelta(weeks=1)
 
+
 # Default celery config is to use SQLA as a broker, in a production setting
 # you'll want to use a proper broker as specified here:
 # https://docs.celeryq.dev/en/stable/getting-started/backends-and-brokers/index.html
@@ -1185,7 +1184,6 @@ BLUEPRINTS: list[Blueprint] = []
 #   )
 # pylint: disable-next=unnecessary-lambda-assignment
 TRACKING_URL_TRANSFORMER = lambda url: url
-
 
 # customize the polling time of each engine
 DB_POLL_INTERVAL_SECONDS: dict[str, int] = {}
@@ -1530,7 +1528,6 @@ SSL_CERT_PATH: str | None = None
 # pylint: disable-next=unnecessary-lambda-assignment
 SQLA_TABLE_MUTATOR = lambda table: table
 
-
 # Global async query config options.
 # Requires GLOBAL_ASYNC_QUERIES feature flag to be enabled.
 GLOBAL_ASYNC_QUERY_MANAGER_CLASS = (
@@ -1687,7 +1684,6 @@ class ExtraDynamicQueryFilters(TypedDict, total=False):
 
 EXTRA_DYNAMIC_QUERY_FILTERS: ExtraDynamicQueryFilters = {}
 
-
 # -------------------------------------------------------------------
 # *                     DATAAI CUSTOM CONFIG                        *
 # -------------------------------------------------------------------
@@ -1705,16 +1701,16 @@ INDEX_LINK = '/superset/dashboard/home/?standalone=true&expand_filters=0'
 HOME_LINK = '/superset/dashboard/home/?standalone=true&expand_filters=0'
 
 HTML_SANITIZATION_SCHEMA_EXTENSIONS = {
-  "attributes": {
-    "*": ["style", "className"],
-    "a": ["href"],
-    "img": ["src", "width", "height"],
-    "iframe": ["frameborder", "width", "height", [
-        "src",
-        "http://172.188.96.246/superset/dashboard/1/"
-    ]]
-  },
-  "tagNames": ["style", "iframe", "audio", "a", "img"],
+    "attributes": {
+        "*": ["style", "className"],
+        "a": ["href"],
+        "img": ["src", "width", "height"],
+        "iframe": ["frameborder", "width", "height", [
+            "src",
+            "http://172.188.96.246/superset/dashboard/1/"
+        ]]
+    },
+    "tagNames": ["style", "iframe", "audio", "a", "img"],
 }
 
 DEFAULT_FEATURE_FLAGS['DRILL_BY'] = True
@@ -1724,8 +1720,28 @@ DEFAULT_FEATURE_FLAGS['HORIZONTAL_FILTER_BAR'] = True
 WTF_CSRF_ENABLED = True
 HTTP_HEADERS = {"X-Frame-Options": "ALLOWALL"}
 TALISMAN_ENABLED = False
-ENABLE_CORS = False
-CORS_OPTIONS: dict[Any, Any] = {}
+ENABLE_CORS = True
+CORS_OPTIONS = {
+    'supports_credentials': True,
+    'allow_headers': [
+        'X-CSRFToken', 'Content-Type', 'Origin', 'X-Requested-With', 'Accept', 'Access-Control-Allow-Origin'
+    ],
+    'resources': [
+        '/superset/csrf_token/',
+        '/api/v1/formData/',
+        '/superset/explore_json/',
+        '/api/v1/query/',
+        '/superset/fetch_datasource_metadata/'
+        '/superset/explore_json/',
+        '/superset/log/'
+    ],
+    'origins': [
+        'rapor.test'
+        , '172.17.0.1:8088'
+        , '0.0.0.0:8088'
+        , '*'
+    ],
+}
 # CORS_OPTIONS = {
 #     'supports_credentials': True,
 #     'allow_headers': [
